34a5b1de7d034b0c16034c1a2b1af9cc44b1f84d
Ashton Hellwig <ahellwig.dev@gmail.com>
2020-04-24 04:01:49 -0600

    script lib

--- scripts/lib/lib.sh
+++ scripts/lib/lib.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 ################################################################################
-# Print messages to STDERR.
+# Print warning/error messages to STDERR.
 ################################################################################
 function eprintlog() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+
+  if [[ "${1}" == "error" ]]; then
+    if [[ "${color_support}" -eq 1 ]]; then
+      echo "\033[0;31m[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $2\033[0m" >&2
+    else
+      echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $2" >&2
+    fi
+  fi
 }
 
 ################################################################################
@@ -12,6 +19,96 @@ function eprintlog() {
 # Returns:
 #   Path of the currently file this function is called in as a string.
 ################################################################################
-function __script_src_dir__() {
+function __SCRIPT_SRC_DIR__() {
   cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd
+}
+
+function __log_msg() {
+  # Declare options
+  declare _opt_USE_COLOR
+  declare _opt_MESSAGE
+
+  # Check color support
+  if [ -t 1 ]; then
+    local OPTIND
+    declare ncolors
+    ncolors=$(tput colors)
+
+    if [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+      declare -A print_colors
+      print_colors=(
+        ["bold"]="$(tput bold)"
+        ["underline"]="$(tput smul)"
+        ["standout"]="$(tput smso)"
+        ["normal"]="$(tput sgr0)"
+        ["black"]="$(tput setaf 0)"
+        ["red"]="$(tput setaf 1)"
+        ["green"]="$(tput setaf 2)"
+        ["yellow"]="$(tput setaf 3)"
+        ["blue"]="$(tput setaf 4)"
+        ["magenta"]="$(tput setaf 5)"
+        ["cyan"]="$(tput setaf 6)"
+        ["white"]="$(tput setaf 7)"
+      )
+
+      _opt_USE_COLOR=true
+    else
+      _opt_USE_COLOR=false
+    fi
+
+    # Process Arguments
+    ## Set defaults
+    _opt_MESSAGE=''
+    declare _opt_MSG_ERROR
+    _opt_MSG_ERROR='false'
+    declare _opt_MSG_INFO
+    _opt_MSG_INFO='true'
+    declare _opt_MSG_SUCCESS
+    _opt_MSG_SUCCESS='false'
+
+    # Parse options
+    while getopts ":esim:" opt; do
+      # shellcheck disable=SC2154
+      case "${opt}" in
+        e)
+          _opt_MSG_INFO='false'
+          _opt_MSG_SUCCESS='false'
+          _opt_MSG_ERROR='true'
+          ;;
+        i)
+          _opt_MSG_ERROR='true'
+          _opt_MSG_SUCCESS='false'
+          _opt_MSG_INFO='true'
+          ;;
+        s)
+          _opt_MSG_ERROR='false'
+          _opt_MSG_INFO='false'
+          _opt_MSG_SUCCESS='true'
+          ;;
+        m)
+          _opt_MESSAGE="${OPTARG}"
+          ;;
+        :)
+          eprintlog "Option: ${OPTARG} requires an argument."
+          ;;
+        \? | *)
+          return 1
+          ;;
+      esac
+    done
+    shift $((OPTIND - 1))
+
+    if [ "$_opt_USE_COLOR" = true ]; then
+      # Option: [-e|--error]
+      if [ "$_opt_MSG_ERROR" = true ]; then
+        echo -e "${print_colors["bold"]}[$(date +'%Y-%m-%dT%H:%M:%S%z')]:${print_colors["normal"]}${print_colors["red"]} $_opt_MESSAGE${print_colors["normal"]}" >&2
+      elif [ "$_opt_MSG_INFO" = true ]; then
+        echo -e "${print_colors["standout"]}[$(date +'%Y-%m-%dT%H:%M:%S%z')]:${print_colors["normal"]}${print_colors["blue"]} $_opt_MESSAGE${print_colors["normal"]}"
+      elif [ "$_opt_MSG_SUCCESS" = true ]; then
+        echo -e "${print_colors["bold"]}[$(date +'%Y-%m-%dT%H:%M:%S%z')]:${print_colors["normal"]}${print_colors["green"]} $_opt_MESSAGE${print_colors["normal"]}"
+      fi
+    else
+      echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $_opt_MESSAGE" >&2
+    fi
+  fi
 }
